54b8f89df214ee32194520ad099df1f01821435f
Jacquelina <jacquelinakhoo@gmail.com>
2026-09-03 12:17:06 +0700

    part 3

--- app/src/main/java/com/example/penjualanrajut/MainActivity.java
+++ app/src/main/java/com/example/penjualanrajut/MainActivity.java
@@ -1,11 +1,34 @@
-package com.example.penjualanmobil;
+package com.example.penjualanrajut;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.penjualanrajut.DataMobilActivity;
+import com.example.penjualanrajut.R;
+
+public class MainActivity extends AppCompatActivity{
+    Button BtnMobil;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_main); }
+        setContentView(R.layout.activity_main);
+
+        BtnMobil=findViewById(R.id.ButtonDatamobil);
+
+
+        BtnMobil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent intent  = new Intent(MainActivity.this, DataMobilActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
